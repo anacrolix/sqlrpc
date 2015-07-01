4baba4ef866c2ffb316561a4d01820cd631eb1ba
Matt Joiner <anacrolix@gmail.com>
2015-07-02 02:30:47 +1000

    Import a Tx locking test from another project

--- sqlrpc_test.go
+++ sqlrpc_test.go
@@ -7,8 +7,10 @@ import (
 	"net/http"
 	"net/rpc"
 	"testing"
+	"time"
 
 	_ "github.com/anacrolix/envpprof"
+	"github.com/bradfitz/iter"
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/stretchr/testify/assert"
 )
@@ -75,4 +77,40 @@ func TestSimple(t *testing.T) {
 	}
 	ra, _ = res.RowsAffected()
 	assert.EqualValues(t, 1, ra)
+}
+
+func TestDatabaseLocked(t *testing.T) {
+	db, _ := sql.Open("sqlrpc", serverAddr)
+	// db.SetMaxOpenConns(1)
+	defer db.Close()
+	db.Exec("create table a(b)")
+	tx, _ := db.Begin()
+	// Lock the database.
+	tx.Exec("insert into a values (42)")
+	// Release the database asynchronously.
+	go func() {
+		time.Sleep(10 * time.Millisecond)
+		tx.Commit()
+	}()
+	started := time.Now()
+	var err error
+	var retries int
+	for retries = range iter.N(100) {
+		// This won't succeed until the other transaction releases its lock.
+		_, err = db.Exec("update a set b=b+1")
+		if err == nil {
+			break
+		}
+		time.Sleep(time.Millisecond)
+	}
+	t.Log(time.Since(started))
+	t.Logf("retries: %d", retries)
+	if err != nil {
+		// Database lock should have been released by now.
+		t.Fatalf("%#v", err)
+	}
+	var b int
+	db.QueryRow("select b from a").Scan(&b)
+	assert.EqualValues(t, 43, b)
+	assert.Nil(t, db.Close())
 }
